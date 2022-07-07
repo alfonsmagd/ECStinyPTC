339fd7521517cb4b326546fb0b9762fc1e987233
alfonsmagd <105669319+alfonsmagd@users.noreply.github.com>
2022-07-07 11:52:45 +0200

    Add files via upload
    install all x11library dependencis to ubuntu 20.04

---
+++ installX11depenciesPTC.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Install all dependencies that tinyPTC need to run. 
+
+sudo apt-get install libx11-dev
+sudo apt-get install libxi-dev 
+sudo apt-get install libxv-dev 
+sudo apt-get install libxext-dev
+
+#add more if your system need it. 
